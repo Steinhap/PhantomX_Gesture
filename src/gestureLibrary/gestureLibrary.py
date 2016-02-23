8dc6a802010166ab2722869fc90cc16619dd6017
Steinhap <steinhap@union.edu>
2016-02-23 18:46:50 -0500

    Update gestureLibrary.py

--- src/gestureLibrary/gestureLibrary.py
+++ src/gestureLibrary/gestureLibrary.py
@@ -148,7 +148,7 @@ class controllerGUI(wx.Frame):
     	
 	shoulder_lift = 0
 	elbow_flex = 0
-	if msg in ["wave", "Wave", "WAVE"]:
+	if msg == "wave":
 		arm_shldr_r.publish(1.0)
 		arm_shldr_l.publish(-1.0)
 		arm_shldr_r.publish(-1.0)
