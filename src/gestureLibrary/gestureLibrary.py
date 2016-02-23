11093255da617b2ad6abf6698e084a412a0824f3
Steinhap <steinhap@union.edu>
2016-02-23 18:42:24 -0500

    Update gestureLibrary.py

--- src/gestureLibrary/gestureLibrary.py
+++ src/gestureLibrary/gestureLibrary.py
@@ -138,13 +138,13 @@ class controllerGUI(wx.Frame):
                 p.publish(0.0)
 
     def stateCb(self, msg):
-    	arm_elbow_r= rospy.Publisher('arm_elbow_flex_joint_right/command', Float64, queue_size=5)
-    	arm_elbow_l= rospy.Publisher('arm_elbow_flex_joint_left/command', Float64, queue_size=5)
-    	arm_shldr_l= rospy.Publisher('arm_shoulder_lift_joint_left/command', Float64, queue_size=5)
-    	arm_shldr_r= rospy.Publisher('arm_shoulder_lift_joint_right/command', Float64, queue_size=5)
-    	wrist_flx= rospy.Publisher('arm_wrist_flex_joint/command', Float64, queue_size=5)
-    	gripper= rospy.Publisher('gripper_joint/command', Float64, queue_size=5)
-    	arm_shldr_pan= rospy.Publisher('arm_shoulder_pan_joint/command', Float64, queue_size = 5)
+    	arm_elbow_r= rospy.Publisher('/arm_elbow_flex_joint_right/command', Float64, queue_size=5)
+    	arm_elbow_l= rospy.Publisher('/arm_elbow_flex_joint_left/command', Float64, queue_size=5)
+    	arm_shldr_l= rospy.Publisher('/arm_shoulder_lift_joint_left/command', Float64, queue_size=5)
+    	arm_shldr_r= rospy.Publisher('/arm_shoulder_lift_joint_right/command', Float64, queue_size=5)
+    	wrist_flx= rospy.Publisher('/arm_wrist_flex_joint/command', Float64, queue_size=5)
+    	gripper= rospy.Publisher('/gripper_joint/command', Float64, queue_size=5)
+    	arm_shldr_pan= rospy.Publisher('/arm_shoulder_pan_joint/command', Float64, queue_size = 5)
     	
 	shoulder_lift = 0
 	elbow_flex = 0
