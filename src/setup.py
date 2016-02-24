b0c5fae12ffd924dce8c0af24518d91de9515a8b
Steinhap <steinhap@union.edu>
2016-02-23 20:01:48 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    scripts=['src/gestureLibrary'],
+    scripts=['gestureLibrary/gestureLibrary.py'],
     packages=['gestureLibrary'],
     package_dir={'': 'src'},
     )
