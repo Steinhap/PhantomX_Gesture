7dc97e85a628ab7bdfa189c9d375b2b502e67622
Steinhap <steinhap@union.edu>
2016-03-01 18:15:11 -0500

    Update gestureLibrary.py

--- src/gestureLibrary/gestureLibrary.py
+++ src/gestureLibrary/gestureLibrary.py
@@ -29,22 +29,6 @@ gripper= rospy.Publisher('/gripper_joint/command', Float64, queue_size=5)
 arm_shldr_pan= rospy.Publisher('/arm_shoulder_pan_joint/command', Float64, queue_size = 5)
 gsture_pub = rospy.Publisher('/armGesture', String, queue_size = 5)
 
-class servoSlider():
-    def __init__(self, parent, min_angle, max_angle, name, i):
-        self.name = name
-        if name.find("_joint") > 0: # remove _joint for display name
-            name = name[0:-6]
-        self.position = wx.Slider(parent, -1, 0, int(min_angle*100), int(max_angle*100), wx.DefaultPosition, (150, -1), wx.SL_HORIZONTAL)
-        self.enabled = wx.CheckBox(parent, i, name+":")
-        self.enabled.SetValue(False)
-        self.position.Disable()
-	self.synched = 0
-
-    def setPosition(self, angle):
-        self.position.SetValue(int(angle*100))
-
-    def getPosition(self):
-        return self.position.GetValue()/100.0
 
 class controllerGUI(wx.Frame):
     TIMER_ID = 1000
@@ -53,24 +37,11 @@ class controllerGUI(wx.Frame):
         wx.Frame.__init__(self, parent, -1, "Gesture Library ", style = wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         sizer = wx.GridBagSizer(5,5)
 
-        # Move Base
-        drive = wx.StaticBox(self, -1, 'Move Base')
-        drive.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD))
-        driveBox = wx.StaticBoxSizer(drive,orient=wx.VERTICAL) 
-        self.movebase = wx.Panel(self,size=(width,width-20))
-        self.movebase.SetBackgroundColour('WHITE')
-        self.movebase.Bind(wx.EVT_MOTION, self.onMove)  
-        wx.StaticLine(self.movebase, -1, (width/2, 0), (1,width), style=wx.LI_VERTICAL)
-        wx.StaticLine(self.movebase, -1, (0, width/2), (width,1))
-        driveBox.Add(self.movebase)        
-        sizer.Add(driveBox,(0,0),wx.GBSpan(1,1),wx.EXPAND|wx.TOP|wx.BOTTOM|wx.LEFT,5)
-        self.forward = 0
-        self.turn = 0
-        self.X = 0
-        self.Y = 0
-        self.button1 = wx.Button(self, id=-1, label='Wave',
-            pos=(8, 8), size=(175, 28))
+        self.button1 = wx.Button(self, id=-1, label='Wave', pos=(8, 8), size=(175, 28))
         self.button1.Bind(wx.EVT_BUTTON, self.button1Click)
+        
+        self.button2 = wx.Button(self, id=-1, label='Button2', pos=(8, 38), size=(175, 28))
+        self.button2.Bind(wx.EVT_BUTTON, self.button2Click)
         
         arm_elbow_r.publish(0.0)
 	arm_elbow_l.publish(0.0)
@@ -109,11 +80,6 @@ class controllerGUI(wx.Frame):
         self.timer = wx.Timer(self, self.TIMER_ID)
         self.timer.Start(50)
         wx.EVT_CLOSE(self, self.onClose)
-
-        # bind the panel to the paint event
-        wx.EVT_PAINT(self, self.onPaint)
-        self.dirty = 1
-        self.onPaint()
 
         self.SetSizerAndFit(sizer)
         self.Show(True)
@@ -157,32 +123,11 @@ class controllerGUI(wx.Frame):
 
     def button1Click(self,event):
     	gsture_pub.publish("Wave")
-   
-    def onPaint(self, event=None):
-        # this is the wx drawing surface/canvas
-        dc = wx.PaintDC(self.movebase)
-        dc.Clear()
-        # draw crosshairs
-        dc.SetPen(wx.Pen("black",1))
-        dc.DrawLine(width/2, 0, width/2, width)
-        dc.DrawLine(0, width/2, width, width/2)
-        dc.SetPen(wx.Pen("red",2))
-        dc.SetBrush(wx.Brush('red', wx.SOLID))
-        dc.SetPen(wx.Pen("black",2))
-        dc.DrawCircle((width/2) + self.X*(width/2), (width/2) - self.Y*(width/2), 5)  
-
-    def onMove(self, event=None):
-        if event.LeftIsDown():        
-            pt = event.GetPosition()
-            if pt[0] > 0 and pt[0] < width and pt[1] > 0 and pt[1] < width:
-                self.forward = ((width/2)-pt[1])/2
-                self.turn = (pt[0]-(width/2))/2 
-                self.X = (pt[0]-(width/2.0))/(width/2.0)
-                self.Y = ((width/2.0)-pt[1])/(width/2.0)        
-        else:
-            self.forward = 0; self.Y = 0
-            self.turn = 0; self.X = 0
-        self.onPaint()          
+    	
+    	
+    def button2Click(self, event):
+    	gsture_pub.publish("Point Left")
+    	
         
 
     	
